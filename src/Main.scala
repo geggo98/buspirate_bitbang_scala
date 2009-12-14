89ad0988efd17282d16cb43583428f8bcd9e5b02
geggo <stefan.schwetschke+git@googlemail.com>
2009-12-14 23:51:13 +0100

    Cleaned up and formatted code, added code style definition

--- src/Main.scala
+++ src/Main.scala
@@ -10,34 +10,31 @@ import java.io.{OutputStream, InputStream}
  */
 
 object Main {
-  def main(arguments: Array[String]) {
+	def main(arguments: Array[String]) {
 
-    println("Hallo Welt");
-    val portName: String = "/dev/tty.usbserial-A600aSsZ"
-    val portIdentifier: CommPortIdentifier = CommPortIdentifier.getPortIdentifier(portName)
-    if (portIdentifier.isCurrentlyOwned())
-      {
-        println("Error: Port is currently in use")
-      }
-    else
-      {
-        val timeout: Int = 2000
-        // Make sure that /var/lock esists and is writeable for the current user.
-        val commPort: CommPort = portIdentifier.open("buspirate_bitbang", timeout)
+		println("Hallo Welt")
+		val portName: String = "/dev/tty.usbserial-A600aSsZ"
+		val portIdentifier: CommPortIdentifier = CommPortIdentifier.getPortIdentifier(portName)
+		if (portIdentifier.isCurrentlyOwned()) {
+			println("Error: Port is currently in use")
+		} else {
+			val timeout: Int = 2000
+			// Make sure that the directory "/var/lock" exists and is writable for the current user.
+			val commPort: CommPort = portIdentifier.open("buspirate_bitbang", timeout)
 
-        commPort match {
-          case serialPort: SerialPort =>
-            serialPort.setSerialPortParams(115200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE)
+			commPort match {
+				case serialPort: SerialPort =>
+					serialPort.setSerialPortParams(115200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE)
 
-            val in: InputStream = serialPort.getInputStream()
-            val out: OutputStream = serialPort.getOutputStream()
+					val in: InputStream = serialPort.getInputStream()
+					val out: OutputStream = serialPort.getOutputStream()
 
 
-            //serialPort.addEventListener(new SerialReader(in));
-            serialPort.notifyOnDataAvailable(true)
-            println("Serial port opened")
-          case _ => println("Error: Only serial ports are handled by this example.")
-        }
-      }
-  }
+					//serialPort.addEventListener(new SerialReader(in));
+					serialPort.notifyOnDataAvailable(true)
+					println("Serial port opened")
+				case _ => println("Error: Only serial ports are handled by this example.")
+			}
+		}
+	}
 }
